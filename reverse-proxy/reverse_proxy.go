146a3e77b977f97a70369dc5b08a6603e77256f1
Shudipta <alittleprogramming@gmail.com>
2018-04-16 17:22:55 +0600

    add reverse proxy

---
+++ reverse-proxy/reverse_proxy.go
@@ -0,0 +1,41 @@
+package reverse_proxy
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	gw "github.com/shudipta/grpc-booklist/booklist"
+)
+
+var (
+	echoEndpoint = flag.String("echo_endpoint", "localhost:10000", "endpoint of YourService")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterBookListHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func Run() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
